70dd8018f714a2e000ce75d9e8b97bdbb6f294f2
caorpr <caorpr@gmail.com>
2016-01-19 12:42:29 -0700

    Added Controller

---
+++ src/Controller/SecondController.h
@@ -0,0 +1,26 @@
+/*
+ * SecondController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: cand3952
+ */
+
+#ifndef CONTROLLER_SECONDCONTROLLER_H_
+#define CONTROLLER_SECONDCONTROLLER_H_
+
+class AfternoonController
+{
+private:
+	int classCount;
+public:
+	AfternoonController();
+	void setClassCount(int classCount);
+	int getClassCount();
+	void start();
+
+
+};
+
+
+
+#endif /* CONTROLLER_SECONDCONTROLLER_H_ */
